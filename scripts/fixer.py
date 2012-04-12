6c9e1471604b7d14fa2019238d8efc738d473a77
Malin <ahlberg.malin@gmail.com>
2012-04-12 17:59:30 +0200

    scripts for better pagenumber-handeling

--- scripts/fixer.py
+++ scripts/fixer.py
@@ -4,6 +4,7 @@ import re
 import glob
 import os.path
 import xmlparse
+from xml.etree import ElementTree as etree
 
 txt = ''' super
           sträng (bra för lines)'''
@@ -13,21 +14,24 @@ def opener(uri):
     # read file
     inp = open(uri).read()
     (_,path) = os.path.split(uri)
-    out = 'fixedNy/'+encode(path)
-    # fix page numbers
+    out = '../fixedTagged/'+encode(path)
+    # concat tags if they are separated by a newpage
     new = xmlparse.use(inp)
+    # fix page numbers
+    newer = xmlparse.tagPageNo(new)
     # remove ugly tags
-    ok1 = re.sub('ns0:','',new)  
+    ok1 = re.sub('ns0:','',newer)  
     ok  = re.sub(r'&#x009;','',ok1)  
     # write file
     print 'writing to', out
     open(out,'w').write(ok)
 
-files = glob.glob('../filerXNy/*.xml')
+files   = glob.glob('../filerX/*.xml')
+filesNy = glob.glob('../filerXNy/*.xml')
 # ['../filerX/Troja.xml'] # 
 
 def doAll():
-    for uri in files:
+    for uri in files+filesNy:
         t = threading.Thread(target=opener,args=(uri,))
         t.start()
 
@@ -55,7 +59,14 @@ def encode(txt):
 
 dic = {'å':'aa','ä':'ae','ö':'oe',':':'-'}
         
-#trim()
+###trim()
 doAll()
 
 trr = '&#x009;'
+
+def test():
+    inp = open("../fixedTagged/Tekla.xml").read()
+    tree = etree.fromstring(inp)
+    print tree[:10]
+
+
--- scripts/xmlparse.py
+++ scripts/xmlparse.py
@@ -1,5 +1,6 @@
 # -*- coding: utf_8 -*-
 
+import re
 from xml.etree import ElementTree as etree
                         # import qualified fast med exakt rätt namn,
                         # annars använd import Modul as nyttnamn
@@ -8,9 +9,18 @@ fil  =  "../filerX/Alexius.xml"
 
 testxml = '''
           <doc>
-          <para><inline font-size="12.00"
+          <para1><inline1 font-size="12.00"
           font-style="Times"># 219 jak h&#xF6;rde aaf tinom wisdom och deylighet sakth,
-          </inline></para>
+          </inline1></para1>
+          <para2><inline2 font-size="12.00"
+          font-style="Times"># 220 två
+          </inline2></para2>
+          <para3><inline3 font-size="12.00"
+          font-style="Times"># 221 tre
+          </inline3></para3>
+          <para3><inline3 font-size="12.00"
+          font-style="Times">221 tre
+          </inline3></para3>
           </doc>
             '''
 
@@ -19,8 +29,9 @@ prefix = "{http://rtf2xml.sourceforge.net/}"
 def use(xmls):
     etree.register_namespace('',"http://rtf2xml.sourceforge.net/")
     tree = etree.fromstring(xmls)
-    movePageNom(tree.find(prefix+'body'))
-    return etree.tostring(tree)
+    (new,_) = concatTags(tree)
+    #movePageNom(tree.find(prefix+'body'))
+    return etree.tostring(new)
 
 val = None
 def movePageNom(tree):
@@ -44,6 +55,66 @@ def movePageNom(tree):
               #print "in tag",elem.tag,"setting var to",p
               val = p
 
+def test():
+    (t,_) = concatTags(etree.fromstring(testxml))
+    print etree.tostring(t)
+
+def concatTags(tree,p = None):
+    pOld = p
+    for elem in tree:
+      # if the last tag is appendable and if this start with # ..
+      # obs flytta ej text uppåt!!
+      if p!=None and elem.text and elem.text.strip()[0:1]=='#':
+         # then move text to last element and empty this
+         p.text = p.text +' '+ elem.text 
+         elem.text = ""
+      # else, check whether this element is appendable
+      elif elem.text and elem.text.strip():
+         p = elem
+      # then go through inner trees
+      (_,p1) = concatTags(elem,p)
+      p = p1
+    return (tree,p)
+
+
+rex2 = re.compile(r"""(\[\s*&\#x2021;&\#x2021;\s*[0-9]*[abrv]*\s*\])|(&\#x2021;&\#x2021;\s*[0-9]*[abrv])|
+                      (\[\s*&\#8225;&\#8225;\s*[0-9]*[abrv]*\s*\]) |(&\#8225;&\#8225;\s*[0-9]*[abrv])""",re.X)
+def tagPageNo(tree):
+         re1 = r'[ >]#\s*[0-9]*'
+         re2 = re.compile(r"""(\[\s*&\#x2021;&\#x2021;\s*[0-9]*[abrv]*\s*\])|(&\#x2021;&\#x2021;\s*[0-9]*[abrv])|
+                      (\[\s*&\#8225;&\#8225;\s*[0-9]*[abrv]*\s*\]) |(&\#8225;&\#8225;\s*[0-9]*[abrv])""",re.X)
+         i = 0
+         intag = "<>"
+         def insert(txt,pos,pos2,elem):
+             return txt[:pos]+elem+txt[pos2:]
+         def inserts(tree,typ,reg,i):
+               newtree = tree
+               while True:
+                 m = re.search(reg,newtree[i:])
+                 if m:
+                    (st,end) = m.span()
+                    num      = extractNo(m.group())
+                    newelem = makeelem(typ,num,m.group()[:1])
+                    newtree = insert(newtree,st+i,end+i,newelem)
+                    i = i+end+len(newelem)
+                 else: break
+               return newtree
+
+         t1 = inserts(tree,"pagenum",re1,0)
+         t2 = inserts(t1,"hskrpagenum",re2,0)
+         return t2
+
+
+def makeelem(tag,att,opt):
+    if opt=='>':
+       x = '>'
+    else: x = ''
+    return x+'<num '+tag+'=\"'+att+'\" />'
+
+def extractNo(s):
+    ok  = re.sub(hskTag,'',s)  
+    return ''.join(filter(lambda x: x.isdigit() or x.isalpha(),ok))
+    
 
 def pageN(elem):
     if elem.text:
@@ -61,6 +132,13 @@ def pageN(elem):
        return x
     else: return None
               
+# TODO
+#tag1 = r'[ >]#\s*[0-9]*'
+##innertag2 = ""
+#innertag2 = re.compile("""(&\#x2021;&\#x2021;|&\#8225;&\#8225;)  # the alternative '#'-sign
+#                           \s*[0-9]*[abrv]*                       # the page number""",re.X)
+#tag2 = re.compile(r'(\[\s*'+innertag2+'\s*\])|'+innertag2)
 
+hskTag = r'(&#x2021;&#x2021;)|(&#8225;&#8225;)' 
 utfilen = "kalas.xml"
-open(utfilen,'w').write(use(open(fil).read()))
+#open(utfilen,'w').write(use(open(fil).read()))

39f73d867c48ee7a0eeb023f78f58d24bb5d5c1e
Malin <ahlberg.malin@gmail.com>
2012-04-17 18:14:05 +0200

    titles and years

--- scripts/checker.py
+++ scripts/checker.py
@@ -1,22 +1,24 @@
 # -*- coding: utf_8 -*-
-
-# kolla om title finns först i texten, skriv ut filnamn annars
-# läsa in title och year från lista och lägga till i fil
-# kolla om filnamnet finns med i en av sections-mapparna, skriv ut annars
-
-
 import re
+import glob
+import threading
 from xml.etree import ElementTree as etree
 
 # checks if the file contains the title elsewhere
-def containsTitle(fil,errs):
+def containsTitle(fil):
+    print "start on",fil
     xml = open(fil,'r').read()
+    etree.register_namespace('',"http://rtf2xml.sourceforge.net/")
     tree = etree.fromstring(xml)
-    tit = tree.find(prefix+'title').text()
+    info = tree.find(prefix+'preamble').find(prefix+'doc-information')
+    tit1 = info.find(prefix+'title')
+    tit = tit1.text
     text = findtext(tree.find(prefix+'body'))
     if not text.startswith(tit):
-       print fil,"Title:",tit,"text",text[:20]
-       errs += fil
+       print fil,"Title:",tit
+       print "text",text[:20],"\n"
+    print "finished",fil
+    return fil
 
 def findtext(tree):
     for e in tree:
@@ -25,6 +27,56 @@ def findtext(tree):
         elif findtext(e):
              return findtext(e)
 
+def checkAll():
+    for uri in files+filesNy:
+        containsTitle(uri)
+#        t = threading.Thread(target=containsTitle,args=(uri,))
+#        t.start()
+
+files   = glob.glob('../filerX/*.xml')
+filesNy = glob.glob('../filerXNy/*.xml')
+
+# TODO remeber to fix encoding when adding titles!
+# for adding info to a list of xml files
+# Each line of the input should contain first the filename
+# and then a list of elements to add
+# Eg. "file.xml year=2012 title=Rubriken" as input
+def readAndAddInfo(uri):
+   text = open(uri,'r').readlines()
+   for uri in text:
+        t = threading.Thread(target=addInfo,args=(uri,))
+        t.start()
+
+# add a new information to a file
+# takes a line such as "file.xml year=2012 title=Rubriken" as input
+# prints the new xml
+def addInfo(line):
+    xs = line.split()
+    etree.register_namespace('',prefix)
+    etree.register_namespace('','ns0')
+    fil = open(xs[0],'r').read()
+    tree = etree.fromstring(fil)
+    info = tree.find(prefix+'preamble').find(prefix+'doc-information')
+    addTag(xs[1:],info)
+    open('ny/'+xs[0],'w').write(tree)
+
+# saves the info (a list of keys and values: ['key1=val1','key2=val2']
+# to the tree. Updates the tags if the already exists, otherwise adds them
+def addTag(info,tree):
+    info = map(lambda x: x.split('='),info)
+    for (tag,val) in info:
+        tit = tree.find(prefix+tag)
+        if tit is not None:
+           tit.text = val
+        else:
+           new = etree.SubElement(tree,tag)
+           new.text = val
+
+
+prefix = "{http://rtf2xml.sourceforge.net/}"
+
+def test2():
+    return addInfo("../filerX/Nic-A.xml title=hej year=44")
 def test():
     return findtext(etree.fromstring(testxml))
 
